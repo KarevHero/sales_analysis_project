4b26448e1420e63202ac3c859a68b2d228d00fc9
KarevHero <askunk86@gmail.com>
2025-09-10 23:35:07 +0300

    Tidied up the structure and wrote a file processing function

---
+++ file_conversion.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def conversion(file_path, start_row=11):
+    df = pd.read_excel(file_path, skiprows=start_row, engine='xlrd')
+
+    df.columns = ["Product name", "Product code", "Product group", "Sales quantity", "Sales amount in USD"]
+
+    df["Date of sale"] = None
+
+    curent_date = None
+    rows_to_keep = []
+
+    for i, row in df.iterrows():
+        name = str(row["Product name"]).strip()
+
+        try:
+            parse_date = pd.to_datetime(name, format="%d.%m.%Y", errors="raise")
+            curent_date = parse_date
+
+        except Exception:
+            df.at[i, "Date of sale"] = curent_date
+            rows_to_keep.append(i)
+
+    df = df.loc[rows_to_keep].reset_index(drop=True)
+
+    return df
